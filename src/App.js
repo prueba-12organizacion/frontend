06e18556b1380feea0831fe8b17167270276c0e9
Franco <francobakal@gmail.com>
2025-06-24 16:06:23 -0300

    Simplify greeting message in App component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hola Mundo! ihjsdafhupgdasf
+          Hola Mundo!
         </p>
       </header>
     </div>
